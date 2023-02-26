6037ae9705a2b8e145ce8ed266caac463466a70d
Juan Narvaez <juand.ns07@gmail.com>
2023-02-25 21:58:53 -0300

    add jwt

--- api/routes/users.js
+++ api/routes/users.js
@@ -8,28 +8,29 @@ import {
 	unsubscribe,
 	update,
 } from "../controllers/user.js";
+import { verifyToken } from "../verifyToken.js";
 
 const router = express.Router();
 
 //update user
-router.put("/:id", update);
+router.put("/:id", verifyToken, update);
 
 //delete user
-router.delete("/:id", update, deleteUser);
+router.delete("/:id", verifyToken, deleteUser);
 
 //get a user
 router.get("/find/:id", getUser);
 
 //subscribe user to a channel
-router.put("/sub/:id", subscribe);
+router.put("/sub/:id", verifyToken, subscribe);
 
 //unsubscribe user from a channel
-router.put("/unsub/:id", unsubscribe);
+router.put("/unsub/:id", verifyToken, unsubscribe);
 
 //like a video
-router.put("/like/:videoId", like);
+router.put("/like/:videoId", verifyToken, like);
 
 //dislike a video
-router.put("/dislike/:videoId", dislike);
+router.put("/dislike/:videoId", verifyToken, dislike);
 
 export default router;
