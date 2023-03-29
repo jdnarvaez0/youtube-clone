f28bff66eca85e417994bd0c44bcfec37ba7db82
Juan Narvaez <juand.ns07@gmail.com>
2023-03-28 23:38:42 -0300

    se agrego login con google y mongodb

--- api/controllers/auth.js
+++ api/controllers/auth.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import User from "../models/User.js";
 import bcrypt from "bcryptjs";
 import { createError } from "../error.js";
@@ -35,6 +34,36 @@ export const signin = async (req, res, next) => {
 			})
 			.status(200)
 			.json(others);
+	} catch (err) {
+		next(err);
+	}
+};
+
+export const googleAuth = async (req, res, next) => {
+	try {
+		const user = await User.findOne({ email: req.body.email });
+		if (user) {
+			const token = jwt.sign({ id: user._id }, process.env.JWT);
+			res
+				.cookie("access_token", token, {
+					httpOnly: true,
+				})
+				.status(200)
+				.json(user._doc);
+		} else {
+			const newUser = new User({
+				...req.body,
+				fromGoogle: true,
+			});
+			const savedUser = await newUser.save();
+			const token = jwt.sign({ id: savedUser._id }, process.env.JWT);
+			res
+				.cookie("access_token", token, {
+					httpOnly: true,
+				})
+				.status(200)
+				.json(savedUser._doc);
+		}
 	} catch (err) {
 		next(err);
 	}
